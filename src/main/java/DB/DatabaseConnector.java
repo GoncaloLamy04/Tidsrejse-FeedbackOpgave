a61c3b6fc9fc6eb76e793a346665cba1941a70a8
GoncaloLamy04 <god001@edu.zealand.dk>
2026-01-30 13:41:03 +0100

    Start

---
+++ src/main/java/DB/DatabaseConnector.java
@@ -0,0 +1,24 @@
+package DB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnector {
+    private static final String URL = "jdbc:mysql://localhost:3306/tidsrejse";
+    private static final String USER = "root";
+    private static final String PASSWORD = "Benfica121!";
+
+    public static Connection connect() throws SQLException{
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+    public static void main (String[] args){
+        try (Connection conn = connect()){
+            System.out.println("Connected to tidsrejse database!");
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.tidsrejsefeedbackopgave {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.example.tidsrejsefeedbackopgave to javafx.fxml;
