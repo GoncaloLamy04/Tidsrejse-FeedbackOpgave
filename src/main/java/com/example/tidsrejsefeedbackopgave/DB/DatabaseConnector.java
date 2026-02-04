a78d92e5f1556b1fef735ed841f2f00313d6a57b
GoncaloLamy04 <god001@edu.zealand.dk>
2026-02-04 19:20:05 +0100

    Update DatabaseConnector.java

--- src/main/java/com/example/tidsrejsefeedbackopgave/DB/DatabaseConnector.java
+++ src/main/java/com/example/tidsrejsefeedbackopgave/DB/DatabaseConnector.java
@@ -9,7 +9,7 @@ public class DatabaseConnector {
     // Lokal MySQL-forbindelse (skoleprojekt)
     private static final String URL = "jdbc:mysql://localhost:3306/tidsrejse";
     private static final String USER = "root";
-    private static final String PASSWORD = "Benfica121!";
+    private static final String PASSWORD = "root";
 
     public static Connection connect() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
